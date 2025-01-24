0d07a94116c34f843cf02c21dbc8e89cb1e55a07
Ragib Badaruddin <ragib.badaruddin@bigpayme.com>
2025-01-25 02:21:33 +0800

    implement sign jws

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import { useState } from 'react'
+import { ChangeEventHandler, MouseEventHandler, useState } from 'react'
+
+import { signJWS } from './lib/sign'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [privateKey, setPrivateKey] = useState('')
+  const [header, setHeader] = useState('')
+  const [payload, setPayload] = useState('')
+
+  const [signature, setSignature] = useState('')
+
+  const sign = async () => {
+    const jws = signJWS(privateKey, header, payload)
+    console.log(jws)
+    setSignature(jws)
+  }
+
+  const onChangePrivateKey: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setPrivateKey(e.target.value)
+  }
+
+  const onChangeHeader: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setHeader(e.target.value)
+  }
+
+  const onChangePayload: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    setPayload(e.target.value)
+  }
+
+  const onClickSignButton: MouseEventHandler<HTMLButtonElement> = (_e) => {
+    void sign()
+  }
 
   return (
     <>
@@ -16,18 +45,26 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+      <h1>JWS Signing</h1>
+
+      <h3>Private Key</h3>
+      <textarea rows={30} cols={100} value={privateKey} onChange={onChangePrivateKey} />
+
+      <h3>Header</h3>
+      <textarea rows={10} cols={100} value={header} onChange={onChangeHeader} />
+
+      <h3>Payload</h3>
+      <textarea rows={20} cols={100} value={payload} onChange={onChangePayload} />
+
+      <div style={{ width: 200 }}>
+        <button onClick={onClickSignButton}>
+          Sign
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h3>Signature</h3>
+      <textarea rows={20} cols={100} value={signature} disabled />
     </>
   )
 }
---
+++ src/lib/sign.ts
@@ -0,0 +1,42 @@
+import forge from 'node-forge'
+import { Base64 } from 'js-base64';
+
+function b64UrltoB64(base64url: string): string {
+  return base64url
+    .replace(/-/g, '+')
+    .replace(/_/g, '/')
+}
+
+function b64ToB64Url(base64: string): string {
+  return base64
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+}
+
+function signMessage(privateKey: string, message: string) {
+  const key = forge.pki.privateKeyFromPem(privateKey)
+
+  const md = forge.md.sha256.create();
+  md.update(message, 'utf8');
+
+  const pss = forge.pss.create({
+    md: forge.md.sha256.create(),
+    mgf: forge.mgf.mgf1.create(forge.md.sha256.create()),
+    saltLength: 20,
+  });
+
+  const sign = key.sign(md);
+  return forge.util.encode64(sign)
+}
+
+export function signJWS(privateKey: string, header: string, payload: string) {
+  const headerPart = Base64.encodeURL(JSON.stringify(JSON.parse(header)))
+  const payloadPart = Base64.encodeURL(payload)
+
+  const message = `${headerPart}.${payloadPart}`
+  const signature = signMessage(privateKey, message)
+  const signPart = b64ToB64Url(signature)
+
+  return `${headerPart}.${payloadPart}.${signPart}`
+}
